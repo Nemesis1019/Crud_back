c2c2ff92a3818192270146c4ad506f91000081b2
Nemesis1019 <caac1019@gmail.com>
2024-02-08 16:26:58 -0500

    Ready for render

--- Principal/settings.py
+++ Principal/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Principal.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
